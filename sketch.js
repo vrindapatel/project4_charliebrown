6c8bc2e4f145b9741b3e0abecc713ff6c227d106
vrindapatel <vrinda.patel.010@gmail.com>
2020-12-18 00:50:57 -0500

    update 2

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
 var song = new Audio("song.mp3");
 
 function setup () {
-createCanvas(800,600);
+let canvas = createCanvas(800,600);
+canvas.position(143,35);
 
 }
 
 
 function draw () {
 
-background(123,187,242);
+// background();
 
-function snow (){
+
 fill(255);
 ellipse(240, 240, 80, 80);
-}
+
 
 }
 
