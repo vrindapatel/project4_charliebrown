e2de84a31d2adede22c6a3b511ea9708206e0de3
vrindapatel <vrinda.patel.010@gmail.com>
2020-12-18 12:10:19 -0500

    update 6

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 
 
+var lastPage = 
 
 var song = new Audio("song.mp3");
 var yPos = [];
@@ -43,3 +44,32 @@ function xSong(){
  	song.play();
 }
  
+
+ function setup() { 
+
+				let canvas = createCanvas(800,600);
+				canvas.position(174,50);
+				strokeWeight(4);
+				stroke(51);
+				fill (random (255), random(255), random(255));
+				ellipse (270,500,30,30);
+				ellipse (190,495,30,30);
+				ellipse (240,455,30,30);
+				ellipse (270,655,30,30);
+				ellipse (270,500,30,30);
+			} 
+
+			function draw() {
+
+				function MouseClicked(){
+					strokeWeight(4);
+					stroke(51);
+					fill (random (255), random(255), random(255));
+					ellipse (270,500,30,30);
+					ellipse (190,495,30,30);
+					ellipse (240,455,30,30);
+					ellipse (270,655,30,30);
+					ellipse (50,50,50,50);
+				}
+					
+			}
