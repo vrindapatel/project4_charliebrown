42e48a1b61a700112e3513d2c0b2bc234e1b4490
vrindapatel <vrinda.patel.010@gmail.com>
2020-12-17 23:52:11 -0500

    1st commit

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var song = new Audio("song.mp3");
+
+function setup () {
+createCanvas(800,600);
+
+}
+
+
+function draw () {
+
+background(123,187,242);
+
+function snow (){
+fill(255);
+ellipse(240, 240, 80, 80);
+}
+
+}
+
+
+function xSong(){
+	song.play();
+}
